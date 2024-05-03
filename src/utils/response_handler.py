56c7178853a9c17af1833d504aba5d499036654f
Amogh Kini <amoghkini@gmail.com>
2024-05-04 05:08:00 +0530

    Added support for next_page in response handler

--- src/utils/response_handler.py
+++ src/utils/response_handler.py
@@ -1,17 +1,19 @@
-from typing import Any
+from typing import Any, Optional
 class Response:
     
     def __init__(
         self,
         success: bool = True,
-        data: Any = None,    
-        message: str = '',
-        errors: dict = {}
+        data: Optional[Any] = None,    
+        message: Optional[str] = None,
+        errors: Optional[dict] = None,
+        next_page: Optional[str]= None
     ) -> None:
         self.success = success
         self.data = data
         self.message = message
         self.errors = errors
+        self.next_page = next_page
         
     def to_dict(self) -> dict:
         return self.__dict__
